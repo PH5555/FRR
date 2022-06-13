e2d287e3ad9a91dde349e0a90edb175b09cf5dec
nyeonaa <hyunah0923@naver.com>
2022-06-13 23:33:44 +0900

    refresh

--- src/pages/RoomReservation.js
+++ src/pages/RoomReservation.js
@@ -4,39 +4,45 @@ import { SeatStatus } from "../components/SeatStatus";
 import styled from "styled-components";
 import { getSeatInfo, reserveSeat, deleteSeat } from "../firebase";
 
+
+let SeatData = ['#fff', '#fff', '#fff', '#fff', '#fff', '#fff', '#fff', '#fff', '#fff', '#fff'];
+let deleteName;
+
 export const RoomReservation = () => {
 
-let SeatData =['#fff', '#fff', '#fff', '#fff', '#fff', '#fff', '#fff', '#fff', '#fff', '#fff'];
+  const promise = getSeatInfo();
+  const getData = () => {
+    console.log(promise);
+    promise.then((appData) => {
+      for (let i = 0; i < appData.length; i++) {
+        if (appData[i].seatNumber == '0') {
+          SeatData[0] = "#282828";
+        } if (appData[i].seatNumber == '1') {
+          SeatData[1] = "#282828";
+        } if (appData[i].seatNumber == '2') {
+          SeatData[2] = "#282828";
+        } if (appData[i].seatNumber == '3') {
+          SeatData[3] = "#282828";
+        } if (appData[i].seatNumber == '4') {
+          SeatData[4] = "#282828";
+        } if (appData[i].seatNumber == '5') {
+          SeatData[5] = "#282828";
+        } if (appData[i].seatNumber == '6') {
+          SeatData[6] = "#282828";
+        } if (appData[i].seatNumber == '7') {
+          SeatData[7] = "#282828";
+        } if (appData[i].seatNumber == '8') {
+          SeatData[8] = "#282828";
+        } if (appData[i].seatNumber == '9') {
+          SeatData[9] = "#282828";
+        }
+      }
+      setseatColor({ ...SeatData });
+    });
+  };
 
-const promise = getSeatInfo();
+  useEffect(() => { getData(); }, []);
 
-const getData = () => {
-promise.then((appData) => {
-  for(let i = 0 ; i < appData.length; i++){
-    if(appData[i].seatNumber == 0){
-      SeatData[0] = "#282828";
-    }if(appData[i].seatNumber == 1){
-      SeatData[1] = "#282828";
-    }if(appData[i].seatNumber == 2){
-      SeatData[2] = "#282828";
-    }if(appData[i].seatNumber == 3){
-      SeatData[3] = "#282828";
-    }if(appData[i].seatNumber == 4){
-      SeatData[4] = "#282828";
-    }if(appData[i].seatNumber == 5){
-      SeatData[5] = "#282828";
-    }if(appData[i].seatNumber == 6){
-      SeatData[6] = "#282828";
-    }if(appData[i].seatNumber == 7){
-      SeatData[7] = "#282828";
-    }if(appData[i].seatNumber == 8){
-      SeatData[8] = "#282828";
-    }
-  }
-setseatColor({...SeatData});
-});
-};
-useEffect(getData, []);
 
   const [name, setName] = useState('');
   const [selected, setSelected] = useState('');
@@ -45,14 +51,18 @@ useEffect(getData, []);
 
   const onClickSeat = (event, id) => {
     const changeId = id;
-    if(selected !== '' && selected !== id) {
+    if (selected !== '' && selected !== id) {
       alert("좌석은 1개만 선택 가능합니다.");
       return;
     }
     if (seatColor[id] == "#282828") {
       setSelected(changeId);
       setButton('취소하기');
-      setName(prompt("취소하려면 이름을 입력하세요", ""));
+      deleteName = prompt("취소하려면 이름을 입력하세요", null);
+      setName(deleteName);
+      if (deleteName == null) {
+        setSelected('');
+      }
       return;
     }
     setButton('예약하기');
@@ -60,26 +70,36 @@ useEffect(getData, []);
     newArr[id] = (newArr[id] === '#fff' ? 'red' : '#fff');
     setseatColor(newArr);
     setSelected(changeId);
-    if (seatColor[id] === "#fff"){
+    if (seatColor[id] === "#fff") {
       setSelected('');
     }
-    console.log(seatColor);
   }
-  useEffect(() => {
-
-  }, [seatColor]);
 
   const onClickReservation = () => {
-    if(button == '예약하기'){
-  reserveSeat(name, selected);
-  alert("예약되었습니다. 사용 종료 후 꼭 취소하기를 눌러주세요.")
-    }if(button == '취소하기'){
-  deleteSeat(name, selected);
-  console.log(name);
-  alert("취소되었습니다.");
+    if (button == '예약하기') {
+      if (selected == ''||name == '') {
+        alert("좌석과 이름 모두 입력해주세요.");
+        return;
+      }
+
+      reserveSeat(name, selected);
+      seatColor[selected] = "#282828";
+
+      alert("예약되었습니다. 사용 종료 후 꼭 취소하기를 눌러주세요.")
+    } if (button == '취소하기') {
+      if (selected == ''||name == '') {
+        alert("좌석과 이름 모두 입력해주세요.");
+        return;
+      }
+      deleteSeat(name, selected);
+      alert("취소되었습니다.");
+      seatColor[selected] = "#fff";
+
     }
-    }
-  
+    setSelected('');
+    setName('');
+  }
+
 
   return (
     <Container>
@@ -90,7 +110,7 @@ useEffect(getData, []);
       </Cover>
     </Container>
   );
-  }
+}
 const Container = styled.div`
   display: flex;
   flex-direction: column;
